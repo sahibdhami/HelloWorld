39f9e8c7e843a3c5aaeab104a28dd634ff0f88c2
Rachhpal Singh <rachhpal.singh@rackspace.com>
2021-03-04 18:42:01 -0600

    test one

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace HelloWorld
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Welcome to the World!");
                 });
             });
         }
